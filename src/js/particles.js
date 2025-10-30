c9f02c95fa87ea87a1ca0c3c3e36c92a4e377da5
Ehan Haque <ehann.hoq@gmail.com>
2025-10-29 23:05:53 -0400

    redid entire landing page

---
+++ src/js/particles.js
@@ -0,0 +1,84 @@
+import { Vector2D, magnitude, normalize } from "./vector2D.js";
+
+const canvas = document.getElementById('particles');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+let mousePos = null;
+document.addEventListener('mousemove', (e) => {
+    mousePos = new Vector2D(e.clientX, e.clientY);
+});
+
+const ctx = canvas.getContext("2d");
+
+class Particle {
+    constructor() {
+        this.pos = new Vector2D(Math.random() * canvas.width, Math.random() * canvas.height);
+        this.speed = new Vector2D(Math.random() * 2 - 1, Math.random() * 2 - 1);
+        this.size = Math.random() * 3 + 1;
+    }
+
+    update() {
+        this.pos = this.pos.add(this.speed);
+
+        if (mousePos) {
+            const dist = magnitude(this.pos.sub(mousePos));
+
+            if (dist < 200) {
+                let vec = this.pos.sub(mousePos);
+                vec = normalize(vec).mult(3);
+                this.pos = this.pos.add(vec);
+            }
+        }
+
+        if (this.pos.x < 0)
+            this.pos.x = canvas.width;
+
+        if (this.pos.x > canvas.width)
+            this.pos.x = 0;
+
+        if (this.pos.y < 0)
+            this.pos.y = canvas.height;
+
+        if (this.pos.y > canvas.height)
+            this.pos.y = 0;
+
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.size, 0, 2 * Math.PI)
+        ctx.fillStyle = 'rgb(240, 238, 220)'
+        ctx.fill();
+    }
+}
+
+function animate() {
+    ctx.fillStyle = "rgb(27, 26, 30)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((p) => {
+        p.update();
+        p.draw();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+const particles = [];
+for (let i = 0; i < 100; i++) {
+    particles.push(new Particle());
+}
+
+animate();
+
+
+
+
+
+
---
+++ src/js/vector2D.js
@@ -0,0 +1,32 @@
+export class Vector2D {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(o) {
+        return new Vector2D(this.x + o.x, this.y + o.y);
+    }
+    sub(o) {
+        return new Vector2D(this.x - o.x, this.y - o.y);
+    }
+    mult(m) {
+        return new Vector2D(this.x * m, this.y * m);
+    }
+
+}
+
+export function magnitude(vec) {
+    return Math.sqrt(
+        vec.x * vec.x + vec.y * vec.y
+    );
+}
+
+export function normalize(vec) {
+    let mag = magnitude(vec);
+    mag += 0.01;
+
+    vec.x /= mag;
+    vec.y /= mag;
+
+    return vec;
+}
